f414591fb91d0cf71742040a0fceb52d33576dc6
USER <au_guest@mcghi.mcg.edu>
2019-12-16 19:39:37 -0500

    finished game

--- functionGame2019.py
+++ functionGame2019.py
@@ -14,9 +14,9 @@ WHITE = (255,255,255)
 grid = [] # 2D array (list of lists) for board
 player = pygame.image.load('player.jpg').convert()
 playerCoord = [0,0]
-icon0 = pygame.image.load('icon0.jpg').convert() 
-icon1 = pygame.image.load('icon1.jpg').convert()
-icon2 = pygame.image.load('icon2.jpg').convert()
+icon0 = pygame.image.load('icon3.jpg').convert() 
+icon1 = pygame.image.load('icon1.gif').convert()
+icon2 = pygame.image.load('icon2.gif').convert()
 icon3 = pygame.image.load('icon4.gif').convert()
 
 # 3 - Define game functions ####################################################
@@ -41,7 +41,7 @@ def drawIcon(rw,cl,iconName):
 def updateBoard():
   for row in range(5):
     for column in range(5):
-      color = WHITE            
+      color = WHITE           
       pygame.draw.rect(screen,color,[(margin + width) * column + margin,
                                      (margin + height) * row + margin,
                                       width, height])
